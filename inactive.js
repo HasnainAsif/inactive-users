96bd9143e0372b6242eb208dcec17ffdc451f448
HasnainAsif <63241977+HasnainAsif@users.noreply.github.com>
2020-12-23 10:31:40 +0500

    Update inactive.js

--- inactive.js
+++ inactive.js
@@ -1,24 +1,38 @@
 const user = {
-    name: "Qasim Asad",
-    age: "25",
-    city: "san francisco",
-    lastActivity: new Date().getTime()
-}
+  name: "Qasim Asad",
+  age: "25",
+  city: "san francisco",
+  lastActivity: new Date().getTime(),
+};
 
 const inactiveFirstStage = () => {
-    console.log("User has been inactive for the past 1-2 minutes");
-}
+  console.log("User has been inactive for the past 1-2 minutes");
+};
 
 const inactiveSecondStage = () => {
-    console.log("User has been inactive for the past 3-4 minutes");
-}
+  console.log("User has been inactive for the past 3-4 minutes");
+};
 
 const inactiveThirdStage = () => {
-    console.log("User has been inactive for the past 5-6 minutes");
-}
+  console.log("User has been inactive for the past 5-6 minutes");
+};
 
+let count = 0;
 
 // this is the main method that acts like a worker and runs every 1 minute
-setInterval(function(){
-    // please write your code here
-}, 1000);
+setInterval(function () {
+  count++;
+
+  if (count <= 2) {
+    inactiveFirstStage();
+  } else if (count <= 3 && count > 2) {
+    inactiveSecondStage();
+  } else if (count > 4 && count <= 5) {
+    inactiveThirdStage();
+  }
+
+  if (count == 5) {
+    count = 0;
+  }
+  // please write your code here
+}, 60000);
